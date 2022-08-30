f34a5ff62bc053612632dbb55b5b1782912e3db7
jeffvswanson <jeff.v.swanson@gmail.com>
2022-08-29 20:36:17 -0600

    Correctly patch insert_payee unit test

--- tests/unit/budgeter/test_payee.py
+++ tests/unit/budgeter/test_payee.py
@@ -23,16 +23,19 @@ from budgeter import payee
         pytest.param(
             {"name": "Corp", "e_mail": "abc@built.com", "phone": "123-867-5309"},
             None,
-            marks=pytest.mark.xfail(),
+            marks=pytest.mark.xfail(raises=AttributeError),
             id="error inserting payee",
         ),
     ],
 )
 def test_insert_payee(given, expected):
     # GIVEN the need to insert a payee entity into the database
-    if expected:
-        payee.insert_payee = mock.Mock(return_value=expected)
     # WHEN
-    got = payee.insert_payee(given)
+    if expected:
+        with mock.patch("budgeter.payee.insert_payee", return_value=expected):
+            got = payee.insert_payee(**given)
+    else:
+        with mock.patch("budgeter.payee.budgeter_db.get_db", return_value=None):
+            got = payee.insert_payee(**given)
     # THEN a created status should be provided or an exception should be raised.
     assert got == expected
